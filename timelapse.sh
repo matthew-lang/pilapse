3db6558cbf1a8cd09ecf19b57b094656d6f0e467
Matthew Lang <matthew-lang@users.noreply.github.com>
2016-02-28 11:27:57 -0500

    Update timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -12,14 +12,15 @@ if [ "$SLEEP_TIME" -lt "2" ]; then
     exit
 fi
 echo "Starting Timelapse sleeping $SLEEP_TIME seconds between snapshots"
-mkdir -p ~/campi/timelapse/
+mkdir -p /home/pi/timelapse-photos/
 sleep 10
 while [ 1 ]
 do
     DATE=`date +%Y%m%d%H%M%S`
-    echo $DATE Taking Picture
-    #streamer -f jpeg -o ~/campi/timelapse/${DATE}-photo.jpeg -s 1920x1080 &
-    fswebcam -r 1920x1080 --jpeg 100 -D 0 --no-overlay --no-timestamp --no-title --no-underlay --no-banner /home/pi/campi/timelapse/${DATE}-photo.jpg
+    #echo $DATE Taking Picture
+    
+    #streamer -f jpeg -o /home/pi/timelapse-photos/${DATE}-photo.jpeg -s 1920x1080 &
+    fswebcam -r 1920x1080 --jpeg 100 -D 0 --no-overlay --no-timestamp --no-title --no-underlay --no-banner /home/pi/timelapse-photos/${DATE}-photo.jpg
     sleep $SLEEP_TIME
 done
 
