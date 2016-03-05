c226d14585e800fa697b990a9b20f4767e3c10f1
Matthew Lang <matthew-lang@users.noreply.github.com>
2016-03-05 13:44:50 -0500

    Now stores each timelapse session in a zip file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install fswebcam vim upstart git zip -y
+cd /home/pi/
+git clone https://github.com/matthew-lang/timelapse-pi.git
+chmod +x /home/pi/timelapse-pi/timelapse.sh
+cp /home/pi/timelapse-pi/timelapse.conf /etc/init/timelapse.conf
+chmod -R pi:pi /home/pi/*
--- timelapse.sh
+++ timelapse.sh
@@ -4,22 +4,47 @@ if [[ $# -eq 0 ]]; then
     echo "No arguments supplied"
     exit
 fi
-mkdir -p /home/pi/timelapse-photos/
+
 SLEEP_TIME=$1
 
 if [ "$SLEEP_TIME" -lt "2" ]; then
     echo 'You must enter a number larger than 2 for the sleep time'
     exit
 fi
+
+OUTPUT_DIR=/home/pi/timelapse-photos
+INSTALL_LOC=/home/pi/timelapse-pi
+ZIP_DIR=/home/pi/timelapse-zips
+
+function takePicture(){
+    NUM_FILES=$(printf "%06d" $(ls -1 /home/pi/timelapse-photos/ | wc -l))
+    PIC_NAME=${OUTPUT_DIR}/${NUM_FILES}-photo.jpg
+    #streamer -f jpeg -o /home/pi/timelapse-photos/${NUM_FILES}-photo.jpeg -s 1920x1080 &
+    fswebcam -r 1920x1080 --jpeg 100 -D 0 --no-overlay --no-timestamp --no-title --no-underlay --no-banner ${PIC_NAME}
+    zip -j -g ${ZIP_NAME} ${PIC_NAME}
+}
+
+function createEmptyZip(){
+    NUM_ZIPS=$(printf "%06d" $(ls -1 $ZIP_DIR | wc -l))
+    ZIP_NAME=${ZIP_DIR}/zip-${NUM_ZIPS}.zip
+    cp ${INSTALL_LOC}/empty.zip ${ZIP_NAME}
+}
 echo "Starting Timelapse sleeping $SLEEP_TIME seconds between snapshots"
-sleep 10
+
+#mkdir if not exists
+mkdir -p /home/pi/timelapse-photos/
+mkdir -p ${ZIP_DIR}
+
+#delete all 0b files... have to move this
+#find /home/pi/timelapse-photos/ -name "*.jpg" -size 0b -delete
+
+rm -rf $OUTPUT_DIR/*
+createEmptyZip
+
+sleep 1
 while [ 1 ]
 do
-    DATE=`date +%Y%m%d%H%M%S`
-    #echo $DATE Taking Picture
-    
-    #streamer -f jpeg -o /home/pi/timelapse-photos/${DATE}-photo.jpeg -s 1920x1080 &
-    fswebcam -r 1920x1080 --jpeg 100 -D 0 --no-overlay --no-timestamp --no-title --no-underlay --no-banner /home/pi/timelapse-photos/${DATE}-photo.jpg
+    takePicture &
     sleep $SLEEP_TIME
 done
 
